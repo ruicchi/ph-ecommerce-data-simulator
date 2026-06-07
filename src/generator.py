ba8101f45f897ee3f77ca6f34a666e25043761de
ruicchi <ihategmpls@gmail.com>
2026-06-07 20:54:04 +0800

    feat: new generator for sessions, aligned on users

--- src/generator.py
+++ src/generator.py
@@ -17,6 +17,20 @@ SIM_CONFIG = {
     "channel_weights": [0.40, 0.30, 0.20, 0.10],
     # session config
     "peak_hours": [13, 21],
+    "session_base_min_seconds": 10,
+    "average_dropoff": 5,
+    "session_count": [1, 2, 3, 4, 5, 8, 15],
+    "session_count_weights": [0.65, 0.15, 0.08, 0.05, 0.04, 0.02, 0.01],
+    "burst_session_weights": [0.70, 0.30],
+    "os_distribution": ["Android", "iOS", "Windows", "macOS"],
+    "os_weights": [0.40, 0.25, 0.20, 0.15],
+    "android_software_version": ["14", "13", "12", "11"],
+    "android_software_version_weights": [0.5, 0.3, 0.15, 0.05],
+    "ios_software_version": ["17.3", "16.5", "15.2"],
+    "ios_software_version_weights": [0.6, 0.3, 0.1],
+    "windows_software_version": ["11", "10"],
+    "windows_software_version_weights": [0.7, 0.3],
+    "macos_software_version": ["Sonoma", "Ventura", "Monterey"],
     "session_base_max_seconds": 300,
     "savviness_time_reduction": 150,
     "android_error_rate": 0.05,
@@ -25,33 +39,33 @@ SIM_CONFIG = {
 }
 
 
-def generate_users(num_users):
-    print(f"Generating {num_users} users")
+def generate_users(total_users: int):
+    print(f"Generating {total_users} users")
 
     # user primary keys
     user_ids = []
-    for _ in range(num_users):
+    for _ in range(total_users):
         new_id = str(uuid.uuid4())
         user_ids.append(new_id)
 
     # assigns acquisition channel
     acq_channel = rng.choice(
-        SIM_CONFIG["channels"], size=num_users, p=SIM_CONFIG["channel_weights"]
+        SIM_CONFIG["channels"], size=total_users, p=SIM_CONFIG["channel_weights"]
     )
 
     # account created timestamps
     base_date = datetime.strptime(SIM_CONFIG["as_of_date"], "%Y-%m-%d")
-    days_ago_array = rng.integers(0, 365, size=num_users)
-    seconds_ago_array = rng.integers(0, 86400, size=num_users)
+    days_ago_array = rng.integers(0, 365, size=total_users)
+    seconds_ago_array = rng.integers(0, 86400, size=total_users)
 
     created_at = []
     for days, seconds in zip(days_ago_array, seconds_ago_array):
         past_date = base_date - timedelta(days=int(days), seconds=int(seconds))
         created_at.append(past_date)
 
-    # engine for latent variables (numpy) | creates a gaussian distribution
-    latent_income_score = rng.normal(loc=0, scale=1.0, size=num_users)
-    latent_tech_savviness = rng.uniform(low=0, high=1.0, size=num_users)
+    # engine for latent variables (numpy) | creates a gaussian distribution | NOTE: this might change, defeats the purpose of funnel optimization analyst
+    latent_income_score = rng.normal(loc=0, scale=1.0, size=total_users)
+    latent_tech_savviness = rng.uniform(low=0, high=1.0, size=total_users)
 
     # turn into dataframe (pandas)
     df_users = pd.DataFrame(
@@ -68,22 +82,136 @@ def generate_users(num_users):
     return df_users
 
 
-def generate_sessions(df_users):
-    print("Generating sessions")
+def generate_sessions(df_users: pd.DataFrame):
+    # calculates total sessions | NOTE: 1 to 5 sessions per user, might change
+    num_users = len(df_users)
+    sessions_per_user = rng.choice(
+        SIM_CONFIG["session_count"],
+        size=num_users,
+        p=SIM_CONFIG["session_count_weights"],
+    )
+    total_sessions = sessions_per_user.sum()
 
+    print(f"Generating {total_sessions} sessions")
+
+    # repeat the parent rows so they line with total_sessions
+    exploded_users = df_users.loc[df_users.index.repeat(sessions_per_user)].reset_index(
+        drop=True
+    )
+
+    # session primary keys
     session_id = []
-    user_id = []
+    for _ in range(total_sessions):
+        new_id = str(uuid.uuid4())
+        session_id.append(new_id)
+
+    # parent foreign keys
+    user_id = exploded_users["user_id"].tolist()
+
+    # session start timestamps (bimodal diurnal peak hours) | NOTE: random days is only limited to 30
+    parent_created_at = exploded_users["account_created_at"].tolist()
+    base_date = datetime.strptime(SIM_CONFIG["as_of_date"], "%Y-%m-%d")
+
+    lifespan_days = [(base_date - created).days for created in parent_created_at]
+
+    is_burst_session = rng.choice(
+        [True, False], size=total_sessions, p=SIM_CONFIG["burst_session_weights"]
+    )
+
+    burst_days = rng.exponential(
+        scale=SIM_CONFIG["average_dropoff"], size=total_sessions
+    )
+    loyalty_days = rng.uniform(low=0, high=lifespan_days, size=total_sessions)
+
+    raw_days = np.where(is_burst_session, burst_days, loyalty_days)
+
+    random_days_array = np.minimum(raw_days, lifespan_days).astype(int)
+
+    chosen_peaks = rng.choice(SIM_CONFIG["peak_hours"], size=total_sessions)
+    normal_dist_hours = rng.normal(loc=chosen_peaks, scale=1.0)
+    random_seconds_array = ((normal_dist_hours % 24) * 3600).astype(int)
+
     session_start_time = []
-    session_duration_seconds = []
+    for created_date, days, seconds in zip(
+        parent_created_at, random_days_array, random_seconds_array
+    ):
+        start_date = created_date + timedelta(days=int(days), seconds=int(seconds))
+        session_start_time.append(start_date)
+
+    # session duration | NOTE: exponential decay is based on tech savviness
+    latent_savviness = exploded_users["latent_tech_savviness"].to_numpy()
+    base_duration = rng.exponential(
+        scale=SIM_CONFIG["session_base_max_seconds"], size=total_sessions
+    )
+    savviness_reduction = latent_savviness * SIM_CONFIG["savviness_time_reduction"]
+
+    # no session is shorter than 10 seconds
+    session_duration_seconds = (
+        np.maximum(
+            SIM_CONFIG["session_base_min_seconds"], base_duration - savviness_reduction
+        )
+        .astype(int)
+        .tolist()
+    )
+
+    # device OS versions
+    base_os_array = rng.choice(
+        SIM_CONFIG["os_distribution"],
+        size=total_sessions,
+        p=SIM_CONFIG["os_weights"],
+    )
+
     device_os_version = []
+    for os_type in base_os_array:
+        if os_type == "Android":
+            version = rng.choice(
+                SIM_CONFIG["android_software_version"],
+                p=SIM_CONFIG["android_software_version_weights"],
+            )
+
+        elif os_type == "iOS":
+            version = rng.choice(
+                SIM_CONFIG["ios_software_version"],
+                p=SIM_CONFIG["ios_software_version_weights"],
+            )
+        elif os_type == "Windows":
+            version = rng.choice(
+                SIM_CONFIG["windows_software_version"],
+                p=SIM_CONFIG["windows_software_version_weights"],
+            )
+
+        else:
+            version = rng.choice(
+                SIM_CONFIG["macos_software_version"],
+            )
+
+        device_os_version.append(f"{os_type} {version}")
+
+    # turn into dataframe (pandas)
+    df_sessions = pd.DataFrame(
+        {
+            "session_id": session_id,
+            "user_id": user_id,
+            "session_start_time": session_start_time,
+            "session_duration_seconds": session_duration_seconds,
+            "device_os_version": device_os_version,
+        }
+    )
+
+    print("session table generated!")
+    return df_sessions
 
 
 # TEST: FOR TESTING ONLY
 if __name__ == "__main__":
-    test_df = generate_users(20)
+    test_users_df = generate_users(20)
+    test_sessions_df = generate_sessions(test_users_df)
 
     print("\nTEST: GENERATED USERS")
-    print(test_df)
+    print(test_users_df)
+
+    print("\nTEST: GENERATED SESSIONS")
+    print(test_sessions_df)
 
     print("\nTEST: DATA TYPES")
-    print(test_df.dtypes)
+    print(test_users_df.dtypes)
